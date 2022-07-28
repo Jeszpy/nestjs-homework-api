da338256d60710a1eb1870610f1be17ddabe6c2a
Jezspy <hleb.lukashonak@gmail.com>
2022-07-28 17:14:27 +0300

    start AuthController testing

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,13 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JWTService } from '../jwt/jwt.service';
+import { UserService } from '../user/user.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const mockAuthService = {};
+  const mockJWTService = {};
+  const mockUserService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-    }).compile();
+      providers: [AuthService, JWTService, UserService],
+    })
+      .overrideProvider(AuthService)
+      .useValue(mockAuthService)
+      .overrideProvider(JWTService)
+      .useValue(mockJWTService)
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
   });
