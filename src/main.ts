03e059e741779eacecd0a5b9f9b532029e60f06c
Jeszpy <hleb.lukashonak@gmail.com>
2022-08-05 18:31:33 +0300

    yarn add swagger

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,23 @@ import * as cookieParser from 'cookie-parser';
 import { pipes } from './pipes';
 import { filters } from './filters';
 import { middlewares } from './middlewares';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const swaggerConfig = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   const port = config.get('PORT');
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('sapi', app, document);
+
   app.setGlobalPrefix('/api');
   app.enableCors();
   app.use(cookieParser());
